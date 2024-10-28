09bf3f385bc30fd4fb28d5c3c51a7ecab8b3cc7f
cafali <77307171+cafali@users.noreply.github.com>
2024-10-28 10:52:29 +0100

    mobile page

---
+++ scripts/mobile.js
@@ -0,0 +1,7 @@
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+    window.location.href = "../mobile/mobile.html";
+}
