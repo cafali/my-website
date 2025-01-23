5e850628f73a5d9f5165105da045d949c37fb78b
cafali <77307171+cafali@users.noreply.github.com>
2025-01-23 19:22:33 +0100

    theme switch

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,12 +1,19 @@
 function applyThemeByTime() {
-    var hour = new Date().getHours(); // current hour 24-hour format
+    var hour = new Date().getHours(); // 24h format
     console.log("Current hour:", hour); // log current hour
 
-    if (hour >= 0 || hour < 24) { // time 
-        pixelcrackStyle.disabled = false; // enable 
-        terminalStyle.disabled = true;   // disable 
+    // get css by ID
+    var pixelcrackStyle = document.getElementById('pixelcrack-style');
+    var terminalStyle = document.getElementById('terminal-style');
+
+    // apply correct theme based on time
+    if (hour >= 6 && hour < 18) {
+        pixelcrackStyle.disabled = true; 
+        terminalStyle.disabled = false;    
     } else {
-        pixelcrackStyle.disabled = true;  // disable 
-        terminalStyle.disabled = false;   // enable 
+        pixelcrackStyle.disabled = false;  
+        terminalStyle.disabled = true;   
     }
 }
+
+applyThemeByTime();
