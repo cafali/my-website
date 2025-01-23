44ba175303c7a1b16e3ba1ab5b6a53eba9da47f6
cafali <77307171+cafali@users.noreply.github.com>
2025-01-23 19:14:20 +0100

    minor tweaks

--- scripts/theme.js
+++ scripts/theme.js
@@ -2,7 +2,7 @@ function applyThemeByTime() {
     var hour = new Date().getHours(); // current hour 24-hour format
     console.log("Current hour:", hour); // log current hour
 
-    if (hour >= 18 || hour < 6) { // time 
+    if (hour >= 0 || hour < 24) { // time 
         pixelcrackStyle.disabled = false; // enable 
         terminalStyle.disabled = true;   // disable 
     } else {
